76eb5c03a7bea576cac613f3190815de6911135e
Siegmentation Fault <siegmentationfault@yandex.ru>
2021-08-31 21:58:55 +0700

    LICENSE

--- deps/libvxl.cpp
+++ deps/libvxl.cpp
@@ -1,3 +1,6 @@
+// https://github.com/xtreme8000/libvxl
+// Licensing information can be found at the end of the file.
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -688,3 +691,27 @@ void libvxl_copy_chunk(struct libvxl_map* map, struct libvxl_chunk_copy* copy,
     copy->width = map->width;
     copy->depth = map->depth;
 }
+
+/*
+MIT License
+
+Copyright (c) 2018 ByteBit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
--- deps/libvxl.hpp
+++ deps/libvxl.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// https://github.com/xtreme8000/libvxl
+// Licensing information can be found at the end of the file.
+
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
@@ -227,3 +230,27 @@ size_t libvxl_stream_read(struct libvxl_stream* stream, void* out);
 //! @param y y-coordinate of block
 //! @param z z-coordinate of block
 bool libvxl_map_isinside(struct libvxl_map* map, int x, int y, int z);
+
+/*
+MIT License
+
+Copyright (c) 2018 ByteBit
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
